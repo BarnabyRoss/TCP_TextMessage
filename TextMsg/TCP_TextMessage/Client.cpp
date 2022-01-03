54c08828f7e24c27da2a22c7aadfd6f4b86127f7
BarnabyRoss <chentong0547@outlook.com>
2022-01-03 23:01:51 +0800

    完成客户端类相关槽函数的编写

--- TextMsg/TCP_TextMessage/Client.cpp
+++ TextMsg/TCP_TextMessage/Client.cpp
@@ -11,35 +11,38 @@ void Client::connectTo(QString ip, qint64 port){
 
 qint64 Client::send(const char* data, int len){
 
-
+  return m_client.write(data, len);
 }
 
 qint64 Client::available(){
 
-
+  return m_client.bytesAvailable();
 }
 
 void Client::close(){
 
-
+  m_client.close();
 }
 
 void Client::onConnected(){
 
-
+  qDebug() << "void Client::onConnected()";
 }
 
 void Client::onReadyRead(){
 
-
+  char buf[128] = { 0 };
+  int len = m_client.read(buf, sizeof(buf) - 1);
+  qDebug() << "receive data bytes : " << len;
+  qDebug() << "receive data content : " << buf;
 }
 
 void Client::onBytesWritten(qint64 bytes){
 
-
+  qDebug() << "write bytes : " << bytes;
 }
 
 void Client::onDisconnected(){
 
-
+  qDebug() << "void Client::onDisconnected()";
 }
